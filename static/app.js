954f31a9686e9ee77dbd0c422d337750f7265a19
Eusebio Simango <eusebiosimango14@gmail.com>
2022-06-05 01:46:42 +0200

    client-side

---
+++ static/app.js
@@ -0,0 +1,33 @@
+const arr = document.getElementsByClassName('options')
+
+const handleClick = (event) => {
+	const targetEl = event.target.classList[0]
+	console.log(targetEl)
+	if (targetEl === 'btn-modal') {
+		event.target.parentElement.children[1].classList.toggle('active-modal')
+	}
+
+	if (targetEl === 'edit' || targetEl === 'buy') {
+		const id = event.target.parentElement.getAttribute("data-id")
+		console.log(id)
+
+		switch (targetEl) {
+			case 'edit':
+				// editProduct(id)
+				console.log('editing')
+				break;
+			case 'buy':
+				// buyProduct(id)
+				console.log('buying')
+				break;
+			default:
+				// statements_def
+				break;
+		}
+	}
+}
+
+for(var i = 0, length1 = arr.length; i < length1; i++){
+	arr[i].addEventListener('click', handleClick)
+}
+
